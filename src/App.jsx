813ad2c82f10b1bcf8f5b9e6ab69f1c00dc1da6f
BudosoRonnie <2101054@ub.edu.ph>
2025-09-20 18:16:02 +0800

    .

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ function App() {
   const myElement = <h5>Hello World</h5>
   const myElement2 = <h5>The Result of 5 + 5 is {5 + 5}</h5>
 
-  const name = "Jester John Sales"
-  const age = 25
+  const name = "Ronnie Budoso"
+  const age = 20
 
   return (
     <>
